3014a70bc144910f74adab244f3871add403cf68
DanTappan <Dan.Tappan@gmail.com>
2025-01-05 11:57:37 -0500

    Update kiosk_cfg.py
    fix typo

--- kiosk_cfg.py
+++ kiosk_cfg.py
@@ -4,7 +4,7 @@
 from os import path
 
 html_index = path.abspath(path.join(path.dirname(__file__), 'index.html'))
-pwdfilename=path.abspath(path.join(path.dirname(__file__), './KIOS_PASSWD'))
+pwdfilename=path.abspath(path.join(path.dirname(__file__), './KIOSK_PASSWD'))
 urlfilename=path.abspath(path.join(path.dirname(__file__), './KIOSK_URL.json'))
 
 
