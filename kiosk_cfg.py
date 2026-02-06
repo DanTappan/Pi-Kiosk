aa9d22efd6765e1fb902b8af3b7cf8a2941e63ee
Tueem <tueem@tomatentum.net>
2026-02-06 11:01:06 +0100

    feat(scale): implement scaling (untested)

--- kiosk_cfg.py
+++ kiosk_cfg.py
@@ -6,5 +6,7 @@ from os import path
 html_index = path.abspath(path.join(path.dirname(__file__), 'index.html'))
 pwdfilename=path.abspath(path.join(path.dirname(__file__), './KIOSK_PASSWD'))
 urlfilename=path.abspath(path.join(path.dirname(__file__), './KIOSK_URL.json'))
+scalefilename=path.abspath(path.join(path.dirname(__file__), './KIOSK_SCALE'))
+
 
 
